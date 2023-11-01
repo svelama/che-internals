4e57818c502d13a99d658b149d257d86739f6307
Sankar Velama <svelama@outlook.com>
2023-11-01 10:41:41 +0000

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+# Add current (arbitrary) user to /etc/passwd and /etc/group
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
+    echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
+  fi
+fi
+
+# PROJECTOR_ASSEMBLY_DIR env variable provided by Docker environment
+cd "$PROJECTOR_ASSEMBLY_DIR"/ide/bin || exit
+
+export CHE_PLUGIN_DEV_MODE=true
+export PROJECT_SOURCE=/projects
+
+./ide-projector-launcher.sh
